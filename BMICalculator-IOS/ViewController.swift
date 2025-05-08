181a954eec7bf21f978c2d914f9c2dbff2f13261
Jaime <jaim>
2025-05-08 12:31:50 +0200

    orimer comit

--- BMICalculator-IOS/ViewController.swift
+++ BMICalculator-IOS/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onHeigthChanger(_ sender: UISlider) {
+        print(sender.value)
+    }
+    @IBAction func onWeightChanger(_ sender: UIStepper) {
+        let value = Int(sender.value)
+        weightLabel.text = "\(value) kg"
+        print(sender.value)
+    }
+    
 }
 
